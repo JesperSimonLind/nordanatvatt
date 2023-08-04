e7efaf69b5498c5dc3a0dbe56be7a2a09cca0cb5
PeppZZon <jespersimonlind@gmail.com>
2023-08-04 17:55:54 +0200

    Final fix

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,7 +2,7 @@ import arbetsklader from "../assets/arbetsklader.png";
 import diverse from "../assets/diverse.png";
 import skurmoppar from "../assets/skurmoppar.png";
 import mattor from "../assets/mattor.png";
-import hero from "../assets/Hero.png";
+import hero from "../assets/Hero.jpg";
 import vittvatt from "../assets/vittvatt.png";
 import nordanalogo from "../assets/Nordana-logo.png";
 import about from "../assets/about-image.jpg";
